487f9f53811e36e3c1e5ad5a7e4afcc57a483f51
MisplacedMango <94024360+MisplacedMango@users.noreply.github.com>
2023-09-19 12:43:30 +0200

    Delete ComputerScience3 directory

--- ComputerScience3/src/liveCode.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class liveCode {
-
-        @Test
-        void testAdd() {
-            assertEquals(4, liveCode2.add(2, 2));
-        }
-   }
